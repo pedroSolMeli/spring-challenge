3b2514853e5c6ffc30f4a7118fbe0f1dc7f64474
Pedro Sol <pedro.sol@mercadolivre.com>
2021-12-28 19:21:51 -0300

    Including Repositories (#13)
    * Incluindo ArticleRepository

---
+++ src/main/java/com/mercadolibre/desafiospring/utils/FileUtils.java
@@ -0,0 +1,23 @@
+package com.mercadolibre.desafiospring.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class FileUtils<T> {
+
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    public void writeFile(String filePath, T object) throws IOException {
+        objectMapper.writeValue(new File(filePath), object);
+    }
+
+    public static String GetJsonBodyMock(String filePath) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(filePath));
+        return new String(bytes);
+    }
+
+}
