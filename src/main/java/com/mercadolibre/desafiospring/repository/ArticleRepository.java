36ffee090abd9e4d5f03245070ec79ef5acdb0e4
Pedro Sol <pedro.sol@mercadolivre.com>
2021-12-28 19:08:53 -0300

    criando ArticleRepository

---
+++ src/main/java/com/mercadolibre/desafiospring/repository/ArticleRepository.java
@@ -0,0 +1,33 @@
+package com.mercadolibre.desafiospring.repository;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mercadolibre.desafiospring.utils.FileUtils;
+import org.springframework.stereotype.Repository;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Repository
+public class ArticleRepository {
+
+    private static List<Article> articles = new ArrayList<>();
+    private static final String PATH = "src/main/resources/articles.json";
+    private static FileUtils fileUtils = new FileUtils();
+
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    public static Article createProduto(Article article) throws IOException {
+        articles.add(article);
+        fileUtils.writeFile(PATH, articles);
+        return article;
+    }
+
+    public static List<Article> getProdutos() throws IOException {
+        String jsonString = FileUtils.GetJsonBodyMock(PATH);
+        articles = Arrays.asList(objectMapper.readValue(jsonString, Article[].class));
+        return articles;
+    }
+
+}
