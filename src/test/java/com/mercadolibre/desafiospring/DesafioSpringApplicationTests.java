9df2352b71772fef8f3b2d5eacbc02ef51a78e06
Pedro Sol <pedro.sol@mercadolivre.com>
2021-12-28 14:23:48 -0300

    initial commit

---
+++ src/test/java/com/mercadolibre/desafiospring/DesafioSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.desafiospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
