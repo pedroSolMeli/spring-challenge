c92706675e4199371730dd6c21dbe5cbd48b97fe
emanuellebesckow <emanuelle.besckow@mercadolivre.com>
2021-12-29 18:19:40 -0300

    Feature/order service (#16)
    * criando OrderService
    
    * Continuacao OrderService
    
    * Desenvolvimento de metodos do Services
    
    * continua metodos service order
    
    Co-authored-by: Willy De Jesus Passos <willy.passos@mercadolivre.com>

--- src/main/java/com/mercadolibre/desafiospring/controller/OrderController.java
+++ src/main/java/com/mercadolibre/desafiospring/controller/OrderController.java
@@ -1,46 +1,55 @@
 package com.mercadolibre.desafiospring.controller;
 
-
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+import com.mercadolibre.desafiospring.model.Article;
+import com.mercadolibre.desafiospring.model.Order;
+import com.mercadolibre.desafiospring.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/" + "v1/" + "order")
 public class OrderController {
 
-    @GetMapping(value = "/{id}" ,produces = APPLICATION_JSON_VALUE)
-    @ResponseBody
-    @ResponseStatus(code = HttpStatus.OK)
-    public ResponseEntity<?> findAll(@PathVariable long id){
+	@Autowired
+	OrderService service;
 
-        return null;
-    }
+	@GetMapping(value = "/{id}", produces = APPLICATION_JSON_VALUE)
+	@ResponseBody
+	@ResponseStatus(code = HttpStatus.OK)
+	public ResponseEntity<?> find(@PathVariable long id) {
+		service.findAll();
+		return null;
+	}
 
-//    	@PostMapping(produces = APPLICATION_JSON_VALUE)
-//	@ResponseBody
-//	@ResponseStatus(code = HttpStatus.CREATED)
-//	public  ResponseEntity<?> create(@RequestBody Order order) {
-//
-//		return null;
-//	}
-//
-//	@PutMapping(value = "/{id}", produces = APPLICATION_JSON_VALUE)
-//	@ResponseBody
-//	@ResponseStatus(code = HttpStatus.NO_CONTENT)
-//	public void update(@PathVariable Long id, @RequestBody Order order) {
-//		service.update(id, article);
-//	}
-//
-//
-//	@DeleteMapping(value = "/{id}", produces = APPLICATION_JSON_VALUE)
-//	@ResponseBody
-//	@ResponseStatus(code = HttpStatus.NO_CONTENT)
-//	public void delete(@PathVariable Long id) {
-//		service.delete(id);
-//	}
+	@PostMapping(produces = APPLICATION_JSON_VALUE)
+	@ResponseBody
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public ResponseEntity<?> create(@RequestBody List<Article> listOrder) {
+		BigDecimal total = service.createOrder(listOrder);
+		return ResponseEntity.ok(total);
+	}
+
+
+	@PutMapping(value = "/{id}", produces = APPLICATION_JSON_VALUE)
+	@ResponseBody
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public void update(@PathVariable Long id, @RequestBody Order order) {
+		service.update(id, order);
+	}
+
+
+	@DeleteMapping(value = "/{id}", produces = APPLICATION_JSON_VALUE)
+	@ResponseBody
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		service.delete(id);
+	}
 
 }
-
-
