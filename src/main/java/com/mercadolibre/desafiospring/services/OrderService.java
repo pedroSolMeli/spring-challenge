3ae3ee05b5c39a377f0055d7cb2ce16a582bf0de
Willy De Jesus Passos <willy.passos@mercadolivre.com>
2021-12-29 16:33:38 -0300

    Desenvolvimento de metodos do Services

--- src/main/java/com/mercadolibre/desafiospring/services/OrderService.java
+++ src/main/java/com/mercadolibre/desafiospring/services/OrderService.java
@@ -2,6 +2,7 @@ package com.mercadolibre.desafiospring.services;
 
 import com.mercadolibre.desafiospring.model.Article;
 import com.mercadolibre.desafiospring.model.Order;
+import com.mercadolibre.desafiospring.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,14 +11,18 @@ import java.util.List;
 
 @Service
 public class OrderService {
-
+    @Autowired
+    OrderRepository repository;
 
     public void findAll() {
     }
 
     public void createOrder(List<Article> listOrder) {
         BigDecimal totalList = calculateTotal(listOrder);
-        //Order order = new Order(listOrder, totalList )  ;
+        //Order order = new Order(listOrder, totalList ) ;
+        //Order order = Order.builder().listaPratos(listaPratos).idMesa(idMesa).id(ID).total(totalPratos.get()).build();
+        Order order = Order.builder().products(listOrder).total(totalList).build();
+        repository.createOrder(order);
 
     }
 
