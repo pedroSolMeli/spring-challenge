35ce4e3dbeeba61c406fdd2620d32fe7ae32c266
emanuellebesckow <emanuelle.besckow@mercadolivre.com>
2022-01-03 11:19:04 -0300

    muda package dtos

--- src/main/java/com/mercadolibre/desafiospring/dto/InputArticleToShopDTO.java
+++ src/main/java/com/mercadolibre/desafiospring/dto/InputArticleToShopDTO.java
@@ -1,4 +1,4 @@
-package DTO;
+package com.mercadolibre.desafiospring.dto;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,20 +14,20 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class InputArticleDTO {
+public class InputArticleToShopDTO {
 	private long productId;
 	private String name;
 	private String brand;
 	// quantidade para reqiuisição de compra, não estoque
 	private int quantity;
 
-	public static Article converte(InputArticleDTO dto) {
+	public static Article converte(InputArticleToShopDTO dto) {
 		Article article = Article.builder().brand(dto.getBrand()).name(dto.getBrand()).productId(dto.getProductId())
 				.build();
 		return article;
 	}
 
-	public static List<Article> converteList(List<InputArticleDTO> articles) {
+	public static List<Article> converteList(List<InputArticleToShopDTO> articles) {
 		return articles.stream().map(u -> converte(u)).collect(Collectors.toList());
 	}
 
